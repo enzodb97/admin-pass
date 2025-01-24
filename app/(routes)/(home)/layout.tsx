3ecd2b450c367a3f5c56d925a9b7e5f31103e748
Enzo Daniel Bertolusso <151404661+enzodb97@users.noreply.github.com>
2025-01-24 20:56:44 -0300

    Mobile/Escritorio sidebar
    Ajustando el navbar tanto escritorio como en mobile

--- app/(routes)/(home)/layout.tsx
+++ app/(routes)/(home)/layout.tsx
@@ -1,17 +1,24 @@
-function LayoutRoutes({
-    children,
-}: Readonly<{children: React.ReactNode}>){  
+import { Logo } from "@/components/Shared/Logo/Logo";
+
+function LayoutRoutes({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-   <main className="h-full">
-    <div className="flex justify-between lg:hidden px-6 py-3 items-start bg-slate-950">
+    <main className="h-full">
+      <div className="flex justify-between lg:hidden px-6 py-3 items-start bg-slate-950">
         <div className="py-1 text-white">
-             
-        <h1>Logotype</h1>
+          <Logo />
         </div>
-    </div>
-    {children}  
-   </main>
+        <div>SidebarMobile</div>
+      </div>
+      <div className="flex h-full">
+        <div className="max-w-lg hidden lg:flex h-full w-72 flex-col bg-blue-950 px-4 text-white fixed">
+          <p>sidebar...</p>
+        </div>
+        <div className="w-full lg:pl-72">
+          <div className="p-6">{children}</div>
+        </div>
+      </div>
+    </main>
   );
-}  
+}
 
 export default LayoutRoutes;
---
+++ components/Shared/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export function Logo() {
+  return (
+    <Link href={"/"}>
+        <h1 className="text-lg font-bold ">
+            Save Pass
+        </h1>
+    </Link>
+  );
+}
