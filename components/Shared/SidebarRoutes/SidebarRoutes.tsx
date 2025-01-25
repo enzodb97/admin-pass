12990370b83531f58e6eb2877778dd51c76d9a16
Enzo Daniel Bertolusso <151404661+enzodb97@users.noreply.github.com>
2025-01-25 19:16:36 -0300

    cargamos los item de Element
    en el sidebar cargamos los item de Element

--- components/Shared/SidebarRoutes/SidebarRoutes.tsx
+++ components/Shared/SidebarRoutes/SidebarRoutes.tsx
@@ -1,20 +1,34 @@
-"use client"
+"use client";
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
-  import { BarChart, DoorClosed, House, Icon, RectangleEllipsis } from "lucide-react"
-  import Link from "next/link";
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  BarChart,
+  DoorClosed,
+  House,
+  Icon,
+  RectangleEllipsis,
+} from "lucide-react";
+import Link from "next/link";
 import SingleItem from "../SingleItem/SingleItem";
-import { dataSidebarElements, dataSidebarConfiguration } from "./SidebarRoutes.data";
+import {
+  dataSidebarElements,
+  dataSidebarConfiguration,
+} from "./SidebarRoutes.data";
 function SidebarRoutes() {
   return (
     <div>
       <SingleItem href="/" label="Homepage" icon={House} />
       {dataSidebarElements.map(({ title, icon: Icon, children }) => (
-        <Accordion type="single" collapsible key={title} className="w-full px-2">
+        <Accordion
+          type="single"
+          collapsible
+          key={title}
+          className="w-full px-2"
+        >
           <AccordionItem value="item-1" className="border-b-0">
             <AccordionTrigger>
               <div className="flex gap-2 items-center">
@@ -24,12 +38,24 @@ function SidebarRoutes() {
                 {title}
               </div>
             </AccordionTrigger>
+            <AccordionContent>
+              {children.map(({ item, href, icon: Icon }) => (
+                <div key={item}>
+                  <Link
+                    href={href}
+                    className="px-6 py-2 flex gap-3 items-center hover:bg_blue-100/20 duration-300 transition-all rounded-md"
+                  >
+                    <Icon size={20} />
+                    {item}
+                  </Link>
+                </div>
+              ))}
+            </AccordionContent>
           </AccordionItem>
         </Accordion>
       ))}
     </div>
   );
-  
 }
 
-export default SidebarRoutes
+export default SidebarRoutes;
